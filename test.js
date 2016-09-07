a92dbf774c9e2b4f605fd7f61b3a37bbc1698e92
Ethan Thomas <ethanthomas@Ethans-MacBook-Pro.local>
2016-09-07 16:53:38 -0700

    Adds robot hook

--- test.js
+++ test.js
@@ -2,6 +2,27 @@ var xtorrent = require("xtorrent");
 var express = require("express");
 var app = express();
 
+app.get("/robot", function(req, res){
+  xtorrent.search({query:"Mr robot"}).then(function (data) {
+      var resultArray = new Array();
+      for (d in data) {
+          if (data[d].title.includes("E10") == true) {
+            resultArray.push(" " + data[d].title);
+          }
+      }
+      if (resultArray.length == 0) {
+        res.send("No new episodes yet :(");
+      } else {
+        console.log("Result array: " + resultArray);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        for (r in resultArray) {
+          res.write("Found Episode: " + resultArray[r] + "\n");
+        }
+        res.end();
+      }
+  });
+});
+
 app.get("/bachelor", function(req, res){
   xtorrent.search({query:"Bachelor in paradise"}).then(function (data) {
       var resultArray = new Array();
