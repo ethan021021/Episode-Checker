78a422858c8f339a1ba9a6f058b28243945af903
Ethan Thomas <ethan021@me.com>
2016-09-06 18:27:07 -0700

    Initial

---
+++ test.js
@@ -0,0 +1,28 @@
+var xtorrent = require("xtorrent");
+var express = require("express");
+var app = express();
+
+app.get("/bachelor", function(req, res){
+  xtorrent.search({query:"Bachelor in paradise"}).then(function (data) {
+      var resultArray = new Array();
+      for (d in data) {
+          if (data[d].title.includes("E11") == true) {
+            resultArray.push(" " + data[d].title);
+          }
+      }
+      if (resultArray.length == 0) {
+        res.send("No new episodes yet :(");
+      } else {
+        console.log("Result array: " + resultArray);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        for (r in resultArray) {
+          res.write("Found Episode: " + resultArray[r] + "\n");
+        }
+        res.end();
+      }
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Started!")
+});
